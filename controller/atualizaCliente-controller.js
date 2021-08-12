8c01110cfce98bed6d33d43ea54defed6a333c4f
Fernando-Escritorio <fernando.takashi12@gmail.com>
2021-08-12 13:29:26 -0300

    Exibindo dados para edição

--- controller/atualizaCliente-controller.js
+++ controller/atualizaCliente-controller.js
@@ -11,3 +11,13 @@ clienteService.editaCliente(id).then((dados) => {
   inputNome.value = dados.nome;
   inputEmail.value = dados.email;
 });
+
+const formulario = document.querySelector('[data-form]')
+
+formulario.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    clienteService.atualizaCliente(id, inputNome.value, inputEmail.value).then( () => {
+        window.location.href = "../screens/edicao_concluida.html"
+    })
+})
--- service/cliente-service.js
+++ service/cliente-service.js
@@ -21,7 +21,7 @@ const criaCliente = (nome, email) => {
 
 const removeCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`, {
-    method: "DELETE"
+    method: "DELETE",
   });
 };
 
@@ -29,11 +29,27 @@ const editaCliente = (id) => {
   return fetch(`http://localhost:3000/profile/${id}`).then((resposta) => {
     return resposta.json();
   });
-}
+};
+
+const atualizaCliente = (id, nome, email) => {
+  return fetch(`http://localhost:3000/profile/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      nome: nome,
+      email: email,
+    }),
+  }).then((resposta) => {
+    return resposta.json();
+  });
+};
 
 export const clienteService = {
   listaClientes,
   criaCliente,
   removeCliente,
-  editaCliente
+  editaCliente,
+  atualizaCliente
 };
